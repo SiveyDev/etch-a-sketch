26abeda7663168abae44df63b72af177a54fa266
SiveyDev <sivey@protonmail.com>
2021-10-20 20:47:46 -0400

    Create grid and color changing function

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,27 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i <= 256; i++) {
+for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
     div.textContent = "X";
+    div.classList.add('unit');
     container.appendChild(div);
 }
+
+const grid = document.querySelectorAll('.unit');
+
+grid.forEach(grid => grid.addEventListener('mouseenter', changeColor));
+
+function changeColor(e) {
+    let red = Math.floor((Math.random() * 256) + 1);
+    let blue = Math.floor((Math.random() * 256) + 1);
+    let green = Math.floor((Math.random() * 256) + 1);
+    e.target.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
+}
+
+// for (let i = 0; i < grid.length; i++) {
+//     let red = Math.floor((Math.random() * 256) + 1);
+//     let blue = Math.floor((Math.random() * 256) + 1);
+//     let green = Math.floor((Math.random() * 256) + 1);
+
+//     grid[i].style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
+// }
